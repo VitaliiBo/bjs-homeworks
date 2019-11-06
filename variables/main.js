ff7c61af76d7f8cc470e9ade9fecddbad75d6986
VitaliiBo <boflying1@gmail.com>
2019-11-06 09:16:26 +0300

    Домашнее задание к занятию 1.1 «Базовый синтаксис»

--- variables/main.js
+++ variables/main.js
@@ -1,5 +1,31 @@
-// Задание 1
+/************
+**Задание 1**
+************/
 
-let algebra = 5;
-let geography = 4;
-let physics = 4;
+let algebra = 4;
+let geography = 5;
+let physics = 3;
+
+let summ = (algebra + geography + physics);
+let results = summ / 3;
+
+console.log(results);
+console.log();
+/************
+**Задание 2**
+************/
+
+let myName = 'Виталий';
+let message = 'Привет, мир! Меня зовут ' + myName + '.';
+
+console.log(message);
+console.log();
+/************
+**Задание 3**
+************/
+
+let x = 2, y = 22, z = 0, result;
+
+result = x * y + 5 * z + x - 1;
+
+console.log(result);
