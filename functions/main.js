5984afbba431d34d09ad3bf6da6c6b0a8a56a089
VitaliiBo <boflying1@gmail.com>
2019-11-12 16:51:47 +0300

    functions 1.3 : Задание 2 - 100%

--- functions/main.js
+++ functions/main.js
@@ -1,29 +1,70 @@
 /******************
 ****Задание 1******
 ******************/
-
-function getSolutions( a , b , c ) {
-  let D = b^2 - 4 * a * c;
+function getSolutions( a = 1 , b = 1 , c ) {
+  let D = Math.pow( b , 2 ) - 4 * a * c;
   if ( D < 0 ) {
     return { D: `${D}` };
   } else if ( D === 0 ) {
     let x1 = ( -b ) / ( 2 * a );
-    return { roots: [`${x1}`], D: `${D}` };
+    return { roots: [`${x1}`], D: D };
   } else {
     let x1 = ((-b) + Math.sqrt( D )) / (2 * a);
     let x2 = ((-b) - Math.sqrt( D )) / (2 * a);
-    return { roots: [`${x1}`,`${x2}`], D: `${D}`};
+    return { roots: [`${x1}`,`${x2}`], D: D };
   }
 }
 
 function showSolutionMessage( a , b , c ) {
-
+  let result = getSolutions( a , b , c );
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(`Значение дискриминанта: ${result.D}`);
+  if ( result.D < 0) {
+    console.log(`Уравнение не имеет вещественных корней` + '. I got NO ROOTS, but home was never on the ground!');
+  } else if ( result.D === 0 ) {
+    console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else {
+    console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+  }
 }
 
-console.log(getSolutions(2, 4, 2));
-
-
+showSolutionMessage( 1 , 2 , 3);
+showSolutionMessage( 7 , 20 , -3);
+showSolutionMessage( 2 , 4 , 2);
 
 /******************
-****Задание 1******
+****Задание 2******
+******************/
+function getAverageScore( data ) {
+  let average = [];
+  for (let prop in data) {
+    data[prop] = getArrAvrg( data[prop] );
+    average.push(data[prop]);
+  }
+  data.average = getArrAvrg( average );
+  return data;
+}
+
+function getArrAvrg( arr ) {
+  let summ = 0;
+  for (let i = 0; i < arr.length; i++) {
+    summ += arr[i];
+  }
+  return summ / arr.length;
+}
+
+console.log( getAverageScore({
+  algebra: [ 2, 4, 5, 2, 3, 4 ],
+  geometry: [ 2, 4, 5 ],
+  russian: [ 3, 3, 4, 5 ],
+  physics: [ 5, 5 ],
+  music: [ 2, 2, 6],
+  english: [ 4, 4, 3 ],
+  poetry: [ 5, 3, 4],
+  chemistry: [ 2 ],
+  french: [ 4, 4 ]
+}));
+
+/******************
+****Задание 2******
 ******************/
