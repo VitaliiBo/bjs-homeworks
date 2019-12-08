9ff3ef5cca09312bcb13e4f20e39fbb6b053a62c
VitaliiBo <boflying1@gmail.com>
2019-12-08 14:14:58 +0300

    1/3 Finished

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -1,3 +1,6 @@
+/************************
+*******Задание 1*********
+************************/
 class Weapon {
   constructor(name, attack, durability, range) {
     this.name = name;
@@ -8,11 +11,14 @@ class Weapon {
   }
 
   takeDamage(damage) {
-    this.durability = this.durability - damage;
-    if (this.durability >= 0) {
+    if (damage === undefined) {
       return this.durability;
-    } else {
+    }
+    this.durability = this.durability - damage;
+    if (this.durability <= 0) {
       return this.durability = 0;
+    } else {
+      return this.durability;
     }
   }
   getDamage() {
@@ -37,6 +43,26 @@ class Weapon {
 
 const staff = new Weapon ('Посох', 8, 300, 2);
 const sword = new Weapon ('Меч', 25, 500, 1);
-const dagger = new Weapon ('Кинжал', 5, 300, 1);
+const dagger = new Weapon ('Нож', 5, 300, 1);
 const bow = new Weapon ('Лук', 10, 200, 3);
-const fist = new Weapon ('Кулак', 1, Infinity, 1);
+const fist = new Weapon ('Рука', 1, Infinity, 1);
+
+const longBow = new Weapon ('Длинный лук', 15, 200, 4 );
+const axe = new Weapon ('Секира', 27, 800, 1);
+const stormStaff = new Weapon ('Посох Бури', 10, 300, 3);
+
+/************************
+*******Задание 2*********
+************************/
+
+class Staff extends Weapon {
+  constructor(name, attack, durability, range) {
+    super(name, attack, durability, range);
+
+  }
+}
+
+
+/************************
+*******Задание 3*********
+************************/
