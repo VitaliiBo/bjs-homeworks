7205b342a04bbb125be73d360830d571412b3d08
VitaliiBo <boflying1@gmail.com>
2019-12-08 22:46:40 +0300

    2/3 Finished

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -9,7 +9,6 @@ class Weapon {
     this.range = range;
     this.thirtyPercentDurability = (this.durability / 100) * 30;
   }
-
   takeDamage(damage) {
     if (damage === undefined) {
       return this.durability;
@@ -30,7 +29,6 @@ class Weapon {
       return this.attack;
     }
   }
-
   isBroken() {
     if (this.durability > 0 || this.durability === Infinity) {
       return false;
@@ -38,31 +36,86 @@ class Weapon {
       return true;
     }
   }
-
 }
-
-const staff = new Weapon ('Посох', 8, 300, 2);
-const sword = new Weapon ('Меч', 25, 500, 1);
-const dagger = new Weapon ('Нож', 5, 300, 1);
-const bow = new Weapon ('Лук', 10, 200, 3);
-const fist = new Weapon ('Рука', 1, Infinity, 1);
-
-const longBow = new Weapon ('Длинный лук', 15, 200, 4 );
-const axe = new Weapon ('Секира', 27, 800, 1);
-const stormStaff = new Weapon ('Посох Бури', 10, 300, 3);
-
+// const staff = new Weapon ('Посох', 8, 300, 2);
+// const sword = new Weapon ('Меч', 25, 500, 1);
+// const dagger = new Weapon ('Нож', 5, 300, 1);
+// const bow = new Weapon ('Лук', 10, 200, 3);
+// const fist = new Weapon ('Рука', 1, Infinity, 1);
+//
+// const longBow = new Weapon ('Длинный лук', 15, 200, 4 );
+// const axe = new Weapon ('Секира', 27, 800, 1);
+// const stormStaff = new Weapon ('Посох Бури', 10, 300, 3);
 /************************
 *******Задание 2*********
 ************************/
-
 class Staff extends Weapon {
   constructor(name, attack, durability, range) {
-    super(name, attack, durability, range);
-
+    super('Посох', 8, 300, 2);
   }
 }
-
-
+class Sword extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Меч', 25, 500, 1);
+  }
+}
+class Dagger extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Нож', 5, 300, 1);
+  }
+}
+class Bow extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Лук', 10, 200, 3);
+  }
+}
+class Fist extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Рука', 1, Infinity, 1);
+  }
+}
+class LongBow extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Длинный лук', 15, 200, 4);
+  }
+}
+class Axe extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Секира', 27, 800, 1);
+  }
+}
+class StormStaff extends Weapon {
+  constructor(name, attack, durability, range) {
+    super('Посох Бури', 10, 300, 3);
+  }
+}
 /************************
 *******Задание 3*********
 ************************/
+
+
+class StudentLog {
+  constructor(name) {
+    this.name = name;
+    this.subjects = {};
+  }
+
+  getName(){
+    return this.name;
+  }
+
+  addGrade(grade, subject){
+    if (grade > 0 && grade <= 5 ) {
+      if (subject in this.subjects) {
+        this.subjects[subject].push(grade);
+      } else { this.subjects = {[subject]:[grade]};
+    }
+    } else {
+        console.log(`Вы пытались поставить оценку "${grade}", принимаются только оценки от 1 до 5`);
+        if (!(subject in this.subjects)) {
+          this.subjects = {[subject]:[]}
+        }
+    }
+    return this.subjects[subject].length;
+  }
+}
