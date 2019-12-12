3fc10b691f50516ec7bb8f3bd7622bcfc2dfffbe
VitaliiBo <boflying1@gmail.com>
2019-12-12 22:56:03 +0300

    memoize начало

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,5 +1,3 @@
-
-
 const compareArrays = ( arr1 , arr2 ) => {
   if (Object.keys(arr1).length !== Object.keys(arr2).length) {
     return false;
@@ -10,8 +8,15 @@ const compareArrays = ( arr1 , arr2 ) => {
 
 console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4]));
 
-const memoize = ( fn , limit ) => {
-  return console.log(fn.arguments);
+function memoize( fn , limit ) {
+  return function () {
+    const results = [];
+      const values = Array.from(arguments);
+      console.log(Array.isArray(values));
+
+      
+    }
 }
 
 const summ = ( a , b ) => a + b ;
+const mSum = memoize( summ , 2 );
